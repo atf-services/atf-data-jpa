8af254d75db2484ebc74c4cd8d7c651e94cfcce4
Todd Crone <todd.crone@perceptivesoftware.com>
2014-09-14 17:46:01 -0400

    Add Runner Groovy script.

--- src/main/groovy/pkg/Main.groovy
+++ src/main/groovy/pkg/Main.groovy
@@ -1,14 +1,9 @@
 package pkg
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext
-
 class Main {
     static void main(String[] args) {
-        def context = new AnnotationConfigApplicationContext(Config.class)
-
-        def repo = context.getBean(PersonRepository.class)
-        def person = new Person(firstName: 'Todd', lastName: 'Crone')
-        repo.save(person)
+        def context = ServiceContext.instance
+        def person = context.createPerson('Todd', 'Crone')
         println "Person with ID=${person.id}"
     }
 }
